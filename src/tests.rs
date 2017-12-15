f0d2f2f6e04753136ec786f76c3b252b69378382
Marc Brinkmann <marc@49nord.de>
2017-12-15 18:35:47 +0100

    Wrote stubs for remaining functionality.

--- src/tests.rs
+++ src/tests.rs
@@ -25,3 +25,83 @@ fn test_hidden_debug() {
 
     assert_eq!("...", format!("{}", data));
 }
+
+#[test]
+fn test_as_str() {
+    unimplemented!()
+}
+
+#[test]
+fn test_reveal_str() {
+    unimplemented!()
+}
+
+#[test]
+fn test_new() {
+    unimplemented!()
+}
+
+#[test]
+fn test_as_ref() {
+    unimplemented!()
+}
+
+#[test]
+fn test_reveal() {
+    unimplemented!()
+}
+
+#[test]
+fn test_reveal_into() {
+    unimplemented!()
+}
+
+#[test]
+fn test_map_revealed() {
+    unimplemented!()
+}
+
+#[test]
+fn test_serde() {
+    unimplemented!()
+}
+
+#[test]
+fn test_copy() {
+    unimplemented!()
+}
+
+#[test]
+fn test_clone() {
+    unimplemented!()
+}
+
+#[test]
+fn test_eq() {
+    unimplemented!()
+}
+
+#[test]
+fn test_partial_eq() {
+    unimplemented!()
+}
+
+#[test]
+fn test_ord() {
+    unimplemented!()
+}
+
+#[test]
+fn test_partial_ord() {
+    unimplemented!()
+}
+
+#[test]
+fn test_sync() {
+    unimplemented!()
+}
+
+#[test]
+fn test_send() {
+    unimplemented!()
+}
