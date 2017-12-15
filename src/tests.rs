370d761d69191eec8ab38fa127849ebe6c863ba1
Marc Brinkmann <marc@49nord.de>
2017-12-15 18:26:00 +0100

    Added most critical tests.

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,27 @@
+use super::Secret;
+
+#[derive(Debug)]
+struct PublicStruct {
+    pub secret_field: Secret<String>,
+}
+
 #[test]
-fn it_works() {
-    assert_eq!(2 + 2, 4);
+fn test_hidden_debug_composite() {
+    let data = PublicStruct { secret_field: "THIS-SHOULD-BE-SECRET".to_owned().into() };
+
+    assert_eq!("PublicStruct { secret_field: ... }", format!("{:?}", data));
+}
+
+#[test]
+fn test_hidden_display() {
+    let data = PublicStruct { secret_field: "THIS-SHOULD-BE-SECRET".to_owned().into() };
+
+    assert_eq!("...", format!("{}", data.secret_field));
+}
+
+#[test]
+fn test_hidden_debug() {
+    let data = Secret::new("THIS-SHOULD-BE-SECRET");
+
+    assert_eq!("...", format!("{}", data));
 }
